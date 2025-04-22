4405b593213ed086773bdbb627b92ffdb19215ca
Arun Gopi <arungopi@live.com>
2025-04-22 12:04:53 +0530

    init

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,22 @@
-import { generateRandomColor } from "@/utils";
+// import { generateRandomColor } from "@/utils";
 
 const { PrismaClient } = require("@prisma/client");
 const { fakerDE: faker } = require("@faker-js/faker");
 
 const prisma = new PrismaClient();
+
+function generateRandomColor() {
+  let hexColor = "";
+  do {
+    const randomInt = Math.floor(Math.random() * 16777216);
+
+    hexColor = `#${randomInt.toString(16).padStart(6, "0")}`;
+  } while (
+    hexColor.toLowerCase() === "#ffffff" ||
+    hexColor.toLowerCase() === "#000000"
+  ); // Ensure it’s not white or black
+  return hexColor;
+}
 
 async function seed() {
   console.log("Seeding data...");
